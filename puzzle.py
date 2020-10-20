786230a842bff67a1c6cd7c5ef58fe3914529b71
Adam <adam.mhatre@menloschool.org>
2020-10-20 15:47:08 -0700

    almost working BFS

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,5 @@
 import copy
+import math
 
 def LoadFromFile(filepath):
 	file = open(filepath, "r")
@@ -15,9 +16,10 @@ def LoadFromFile(filepath):
 		listrep.append(y)
 
 	if(len(listrep) > n):
-		return Non
+		return None
 
-	return listrep
+	tuplerep = tuple(map(tuple, listrep))
+	return tuplerep
 
 	file.close()	
 
@@ -57,28 +59,35 @@ def computeNeighbors(state):
 	if (colOfSpace < len(state) - 1):
 		returnlist.append(computeHorizontalState(state, locationOfSpace, 1))
 
-	return returnlist
+	tuplelist = tuple(returnlist)
+	return tuplelist
 
 
 def computeVerticalState(state, locationOfSpace, pos):
-	tempstate = copy.deepcopy(state)
+	tempstate = list(map(list, copy.deepcopy(state)))
 	row, col = locationOfSpace
 
 	num = tempstate[row + pos][col]
 	tempstate[row][col] = num
 	tempstate[row + pos][col] = "*"
-	returnlist1 = [num, tempstate]
-	return returnlist1
+
+	tuplestate = tuple(map(tuple, tempstate))
+
+	returntuple = (num, tuplestate)
+	return returntuple
 
 def computeHorizontalState(state, locationOfSpace, pos):
-	tempstate = copy.deepcopy(state)
+	tempstate = list(map(list, copy.deepcopy(state)))
 	row, col = locationOfSpace
 
 	num = tempstate[row][col + pos]
 	tempstate[row][col] = num
 	tempstate[row][col + pos] = "*"
-	returnlist2 = [num, tempstate]
-	return returnlist2
+
+	tuplestate = tuple(map(tuple, tempstate))
+
+	returntuple = (num, tuplestate)
+	return returntuple
 
 def IsGoal(state):
 	i = 1
@@ -91,56 +100,106 @@ def IsGoal(state):
 				return False
 			if (int(element) != i):
 				return False
-			i = i +1
+			i = i + 1
 
 def BFS(state):
 	frontier = [state]
-	discovered = set(map(tuple, flatten(state)))
+	discovered = set(state)
 
-	parents = {map(tuple, flatten(state)): None}
+	parents = {state: None}
 
 	while frontier:
 		current_state = frontier.pop(0)
 
-		discovered.add(map(tuple, flatten(current_state)))
+		discovered.add(current_state)
 
 		if IsGoal(current_state):
+			states = []
+			states.append(current_state)
 			tiles = []
-			print(parents[map(tuple, flatten(current_state))])
+
 			currentkey = parents[current_state]
+
 			while currentkey != None:
-				tiles.append(currentkey)
-				currentkey = tuple(currentkey).get()
+				states.append(currentkey)
+				currentkey = parents[currentkey]
+
+			i = 1
+			for state in states:
+				if (i <=len(states)):
+					tiles.append(getTile(state, states[i]))
+
 			return tiles
+
 		for neighbor in computeNeighbors(current_state):
-			# print("x")
-			if (map(tuple, neighbor)) not in discovered:
-				print("y")
+			if neighbor[1] not in discovered:
 				frontier.append(neighbor[1])
-				discovered.add((map(tuple, flatten(neighbor))))	
-				parents[map(tuple, flatten(neighbor))] = current_state
+				discovered.add(neighbor[1])	
+				parents[neighbor[1]] = current_state
 
-def flatten(state):
-	list2d = []
-	list = []
-	for row in state:
-		for element in row:
-			list.append(element)
-	list2d.append(list)
-	return list2d
 
+def getTile(state1, state2):
+	for neighbor in computeNeighbors(state1):
+		if neighbor[1] == state2:
+			return neighbor[0]
+
+	return "too long"
+# def get_key(val, parents):
+#     for key, value in parents.items(): 
+#          if val == value: 
+#              return key
+  
+#     return None
+# def flatten(state):
+# 	list2d = []
+# 	list = []
+# 	for row in state:
+# 		for element in row:
+# 			list.append(element)
+# 	list2d.append(list)
+# 	return list2d
+
+
+
+# def flattenNieghbor(nieghborstate):
+# 	state = nieghborstate[1]
+# 	tile = nieghborstate.pop(0)
+# 	list = []
+# 	list2d = []
+# 	list.append(tile)
+# 	for row in state:
+# 		for element in row:
+# 			list.append(element)
+
+# 	list2d.append(list)	
+# 	return list2d
+
+# def unflatten(list):
+# 	length = math.sqrt(len(list))
+# 	list2d = []
+# 	individuallist = []
+# 	i = 0
+# 	for element in list:
+# 		if(i == length):
+# 			i = 0
+# 			list2d.append(individuallist)
+# 			individuallist.clear()
+# 		else:
+# 			individuallist.append(element)
+# 	print(list2d)
+# 	return list2d
 
 def main():
 
 	gamestate = (LoadFromFile("npuzzledata.txt"))
 	print(gamestate)
 	print("\n")
-	DebugPrint(gamestate)
-	print("\n")
+	# DebugPrint(gamestate)
+	# print("\n")
 
 	print(BFS(gamestate))
 	# print(IsGoal(gamestate))
-	#print(computeNeighbors(gamestate))
+	# print(computeNeighbors(gamestate))
 	#print(computeNeighbors(gamestate))
 
 	# list = computeNeighbors(gamestate)
