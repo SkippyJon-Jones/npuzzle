5e1616bb7dd65ca9dd65603faa3b55a49d2a5f50
Adam <adam.mhatre@menloschool.org>
2020-10-24 15:52:40 -0700

    mostly working code with no comments

--- puzzle.py
+++ puzzle.py
@@ -125,9 +125,87 @@ def BFS(state):
 				currentkey = parents[currentkey]
 
 			i = 1
-			for state in states:
-				if (i <=len(states)):
-					tiles.append(getTile(state, states[i]))
+			for stategoal in states:
+				if (i < len(states)):
+					tiles.append(getTile(stategoal, states[i]))
+					i = i + 1
+
+			tiles.reverse()
+
+			return tiles
+
+		for neighbor in computeNeighbors(current_state):
+			if neighbor[1] not in discovered:
+				frontier.append(neighbor[1])
+				discovered.add(neighbor[1])	
+				parents[neighbor[1]] = current_state
+	return None
+
+
+def BidirectionalSearch(state):
+	goalstate = getGoalState(state)
+
+	frontier = [state]
+	frontierGoal = [goalstate]
+
+	discovered = set(state)
+	discoveredGoal = set(goalstate)
+
+	parents = {state: None}
+	parentsGoal = {goalstate: None}
+
+	while frontier or frontierGoal:
+		current_state = frontier.pop(0)
+		current_stateGoal = frontierGoal.pop(0)
+
+		discovered.add(current_state)
+		discoveredGoal.add(current_stateGoal)
+
+		intersection = discovered.intersection(discoveredGoal)
+
+		if(len(intersection) > 0):
+			print(intersection)
+			current_state = intersection.pop()
+			current_stateGoal = current_state
+			states = []
+			states.append(current_state)
+			tiles = []
+
+			currentkey = parents[current_state]
+
+			while currentkey != None:
+				states.append(currentkey)
+				currentkey = parents[currentkey]
+
+			i = 1
+			for prevstate in states:
+				if (i < len(states)):
+					tiles.append(getTile(prevstate, states[i]))
+					i = i + 1
+
+			tiles.reverse()
+
+			statesgoal = []
+			statesgoal.append(current_stateGoal)
+			tilesgoal = []
+
+			currentkeygoal = parentsGoal[current_stateGoal]
+
+			while currentkeygoal != None:
+				statesgoal.append(currentkeygoal)
+				currentkeygoal = parentsGoal[currentkeygoal]
+
+			j = 1
+			for prevstategoal in statesgoal:
+				if (j < len(statesgoal)):
+					tilesgoal.append(getTile(prevstategoal, statesgoal[j]))
+					j = j + 1
+
+
+
+			for x in tilesgoal:
+				tiles.append(x)
+
 
 			return tiles
 
@@ -137,57 +215,88 @@ def BFS(state):
 				discovered.add(neighbor[1])	
 				parents[neighbor[1]] = current_state
 
+		for neighbor in computeNeighbors(current_stateGoal):
+			if neighbor[1] not in discoveredGoal:
+				frontierGoal.append(neighbor[1])
+				discoveredGoal.add(neighbor[1])	
+				parentsGoal[neighbor[1]] = current_stateGoal
+
+
+def getGoalState(state):
+	tempstate = list(map(list, copy.deepcopy(state)))
+	i = 1
+	row = 0
+	col = 0
+	for originalrow in tempstate:
+		for element in originalrow:
+			if(i == (len(tempstate) ** 2)):
+				tempstate[row][col] = "*"
+			else:
+				tempstate[row][col] = str(i)
+				i += 1
+			col += 1
+		col = 0
+		row += 1
+
+	tuplestate = tuple(map(tuple, tempstate))
+
+	return tuplestate
+
+
+def DFS(state):
+	frontier = [state]
+	discovered = set(state)
+
+	parents = {state: None}
+
+	while frontier:
+		current_state = frontier.pop(0)
+
+		discovered.add(current_state)
+
+		if IsGoal(current_state):
+			states = []
+			states.append(current_state)
+			tiles = []
+
+			currentkey = parents[current_state]
+
+			while currentkey != None:
+				states.append(currentkey)
+				currentkey = parents[currentkey]
+
+			i = 1
+			for stategoal in states:
+				if (i < len(states)):
+					tiles.append(getTile(stategoal, states[i]))
+					i = i + 1
+
+			tiles.reverse()
+
+			return tiles
+
+		for neighbor in computeNeighbors(current_state):
+			if neighbor[1] not in discovered:
+				frontier.insert(0, neighbor[1])
+				discovered.add(neighbor[1])	
+				parents[neighbor[1]] = current_state
+	return None
+
+
 
 def getTile(state1, state2):
 	for neighbor in computeNeighbors(state1):
 		if neighbor[1] == state2:
 			return neighbor[0]
 
-	return "too long"
-# def get_key(val, parents):
-#     for key, value in parents.items(): 
-#          if val == value: 
-#              return key
-  
-#     return None
-# def flatten(state):
-# 	list2d = []
-# 	list = []
-# 	for row in state:
-# 		for element in row:
-# 			list.append(element)
-# 	list2d.append(list)
-# 	return list2d
+def swapTiles(tiles, state):
+	newstate = copy.deepcopy(state)
+	for tile in tiles:
+		for neighbor in computeNeighbors(newstate):
+			if neighbor[0] == tile:
+				newstate = neighbor[1]
+	return newstate
 
-
-
-# def flattenNieghbor(nieghborstate):
-# 	state = nieghborstate[1]
-# 	tile = nieghborstate.pop(0)
-# 	list = []
-# 	list2d = []
-# 	list.append(tile)
-# 	for row in state:
-# 		for element in row:
-# 			list.append(element)
-
-# 	list2d.append(list)	
-# 	return list2d
-
-# def unflatten(list):
-# 	length = math.sqrt(len(list))
-# 	list2d = []
-# 	individuallist = []
-# 	i = 0
-# 	for element in list:
-# 		if(i == length):
-# 			i = 0
-# 			list2d.append(individuallist)
-# 			individuallist.clear()
-# 		else:
-# 			individuallist.append(element)
-# 	print(list2d)
-# 	return list2d
 
 def main():
 
@@ -196,8 +305,13 @@ def main():
 	print("\n")
 	# DebugPrint(gamestate)
 	# print("\n")
+	# print(getGoalState(gamestate))
+	print(BidirectionalSearch(gamestate))
 
-	print(BFS(gamestate))
+	# list = ['1', '2', '3', '8', '6', '4', '5', '1', '7', '5', '1', '7', '2', '3', '8', '6', '7', '8', '6', '7', '4', '1', '5', '2', '3', '6', '8', '5', '2', '3', '6']
+	# print(swapTiles(list, gamestate))
+	# print(DFS(gamestate))
+	# print(BFS(gamestate))
 	# print(IsGoal(gamestate))
 	# print(computeNeighbors(gamestate))
 	#print(computeNeighbors(gamestate))
